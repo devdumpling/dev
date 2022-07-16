095bbf93b30af509bdb41eadaaaef8e3dabcedba
Devon Wells <devon.wells@goodrx.com>
2022-07-16 07:01:33 -0400

    Feature about page (#8)
    * rename
    
    * fix(remove about)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,11 +3,11 @@ import Link from 'next/link';
 import { DarkModeToggle } from '../DarkModeToggle';
 
 export const Header = ({
-  title = "Hi, I'm Dev",
-  subtitle = 'Stay a while if you like.',
+  title = "Dev's Journal",
+  subtitle = 'Notes and rants.',
 }) => {
   return (
-    <Paper shadow="xs" p="md" my="sm">
+    <Paper p="md" my="sm">
       <Group position="apart" align="end">
         <Link href="/" passHref>
           <Box>
@@ -17,8 +17,7 @@ export const Header = ({
         </Link>
         <Stack align="flex-end" spacing="xs">
           <DarkModeToggle />
-          <Group position="right">
-            <Anchor href="/about">About</Anchor>
+          <Group position="right">          
             <Anchor href="/posts">Posts</Anchor>
           </Group>
         </Stack>
