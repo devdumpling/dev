26e56abe330a51068e2be28989702950d09590fc
Devon Wells <devon.wellsa@gmail.com>
2022-09-27 09:52:17 -0400

    formatting

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-const withMDX = require("@next/mdx")({
+const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
   options: {
     remarkPlugins: [],
-    rehypePlugins: [],    
-    providerImportSource: "@mdx-js/react",
+    rehypePlugins: [],
+    providerImportSource: '@mdx-js/react',
   },
 });
 module.exports = withMDX({
   // Append the default value with md extensions
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
 });
